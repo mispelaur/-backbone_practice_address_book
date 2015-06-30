ed818a57eedf84a7becd244a41e146f5311d27b0
Lauren Spencer <spencer.lauren.m@gmail.com>
2015-06-30 13:28:34 +0100

    edited seed data

--- js/script.js
+++ js/script.js
@@ -1,7 +1,7 @@
-var contact1 = new addressBookApp.Contact({name: "Lauren Spencer", number: "07858123456"});
-var contact2 = new addressBookApp.Contact({name: "Meredith Spencer", number: "07858123457"});
-var contact3 = new addressBookApp.Contact({name: "Scott Spencer", number: "07858123458"});
-var contact4 = new addressBookApp.Contact({name: "Janet Spencer", number: "07858123459"});
+var contact1 = new addressBookApp.Contact({name: "Lauren", number: "07858000000"});
+var contact2 = new addressBookApp.Contact({name: "Meredith", number: "07858000001"});
+var contact3 = new addressBookApp.Contact({name: "Scott", number: "07858000002"});
+var contact4 = new addressBookApp.Contact({name: "Janet", number: "07858000003"});
 
 addressBookApp.addressBook = new addressBookApp.AddressBook([contact1, contact2, contact3, contact4]);
 
